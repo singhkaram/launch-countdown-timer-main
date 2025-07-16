f7dd8bc3bc00ae63c4d7dafe76cde0268dcc89dd
karamsinghus <karamsing468@gmail.com>
2025-07-16 21:02:14 +0530

    update: design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import FlipCard from "./components/FlipCard/FlipCard";
+import FlipCard from "./components/FlipCard";
 import { Social_icons } from "./data";
 
 function App() {
@@ -35,38 +35,17 @@ function App() {
       <div className="time-container">
         <h1 className="main-title">We're launching soon</h1>
         <div className="flip-cards-container">
-          <FlipCard
-            value={
-              countdown.days.toString().length === 1
-                ? `0${countdown.days}`
-                : countdown.days
-            }
-            text={"Days"}
-          />
-          <FlipCard
-            value={
-              countdown.hours.toString().length === 1
-                ? `0${countdown.hours}`
-                : countdown.hours
-            }
-            text={"Hours"}
-          />
-          <FlipCard
-            value={
-              countdown.minutes.toString().length === 1
-                ? `0${countdown.minutes}`
-                : countdown.minutes
-            }
-            text={"Minutes"}
-          />
-          <FlipCard
-            value={
-              countdown.seconds.toString().length === 1
-                ? `0${countdown.seconds}`
-                : countdown.seconds
-            }
-            text={"Seconds"}
-          />
+          {["days", "hours", "minutes", "seconds"].map((item) => (
+            <FlipCard
+              value={
+                countdown[item].toString().length === 1
+                  ? `0${countdown[item]}`
+                  : countdown[item]
+              }
+              text={item}
+              key={item}
+            />
+          ))}
         </div>
       </div>
 
